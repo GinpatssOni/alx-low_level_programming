47824e3da2b85528a14b0028109bd1080992ffff
OUFRID MOHAMED <mohamedof00@gmail.com>
2023-02-17 15:48:39 -0800

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - program antry point
+  *
+  * Return: Always Return 0
+  */
+
+int main(void)
+{
+	char c;
+
+	c = 97;
+	while (c < 123)
+	putchar(c++);
+	putchar('\n');
+	return (0);
+}
