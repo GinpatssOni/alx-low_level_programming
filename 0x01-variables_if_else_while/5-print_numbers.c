9628fd04dfdb88f0c0047f0a1a60d86b1956a58c
OUFRID MOHAMED <mohamedof00@gmail.com>
2023-02-17 16:23:26 -0800

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - program antry point
+  *
+  * Return: Always Return 0
+  */
+
+int main(void)
+{
+	char c;
+
+	c = 48;
+	while (c < 58)
+	putchar(c++);
+	putchar('\n');
+	return (0);
+}
