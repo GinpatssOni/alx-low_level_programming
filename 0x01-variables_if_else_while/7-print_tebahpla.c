51c55c711e3a64a1196adf52f88db1ee66902382
OUFRID MOHAMED <mohamedof00@gmail.com>
2023-02-17 16:30:13 -0800

    add int c;

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - program antry point
+  *
+  * Return: Always Return 0
+  */
+
+int main(void)
+{
+	int c;
+
+	c = 122;
+	while (c > 96)
+	putchar(c--);
+	putchar('\n');
+	return (0);
+}
