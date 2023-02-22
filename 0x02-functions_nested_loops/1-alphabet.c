417f4d5cfdac79a59624d12313fba8dbadaaec98
OUFRID MOHAMED <mohamedof00@gmail.com>
2023-02-22 07:41:32 -0800

    add 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,17 @@
 #include "main.h"
+/**
+  * print_alphabet - print lowercase alphabets
+  *
+  * Return: nothing
+  */
 
 void print_alphabet(void)
 {
 	char x = 97;
+
 	while (x <= 122)
 	{
-		_putchar(x);
+	_putchar(x++);
 	}
 	_putchar('\n');
 }
