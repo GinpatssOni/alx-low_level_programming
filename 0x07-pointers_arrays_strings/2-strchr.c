c882f1c39c60d6a1eac9506cd84340b50244f4df
mohamedof <mohamedof@debian>
2023-03-07 02:54:52 +0100

    add 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _strchr -  locates a character in a string.
+  * @s: string to search
+  * @c: character to search with
+  * Return: pointer to the first occurrence of the character c in the string s
+  * or NULL if the character is not found.
+  */
+
+char *_strchr(char *s, char c)
+{
+	while (s)
+	{
+	if (*(s++) == c)
+	break;
+	}
+	return (--s);
+}
