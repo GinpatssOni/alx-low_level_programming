5f825743ef1e3202e878b4cb4258bd6e303b765c
mohamedof <mohamedof@debian>
2023-03-07 02:19:17 +0100

    add 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _memset - fill memory with given byte.
+  * @s: pointer to the first byte of memory
+  * @b: byte value to fill with
+  * @n: number of bytes to fill
+  * Return: pointer to s the first byte
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+	n--;
+	*(s+n) = b;
+	}
+	return (*s = b);
+}
